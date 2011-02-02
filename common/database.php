7e788f4c471755196c2dd2a84de70474677fba02
Matt F <mfried@mac.com>
2011-02-02 16:37:19 -0500

    Added Database Query Function
    Signed-off-by: Matt F <mfried@mac.com>

--- common/database.php
+++ common/database.php
@@ -3,4 +3,46 @@
 	date_default_timezone_set('America/New_York');
 
 
+function mySQLQuery($theQuery) {
+		$databaseHost = 'localhost';
+		$databaseUser = 'inventory';
+		$databasePassword = 'inventory';
+		$databaseName = 'sdny_inventory';
+		$databasePort = '3306';
+/* 		$databaseSock = "/tmp/mysql.sock"; */
+		print $theQuery;
+		
+ 		$databaseConnection = mysql_connect($databaseHost, $databaseUser, $databasePassword); 
+ 			
+ 		if ($databaseConnection){
+ 			
+ 			if (mysql_select_db($databaseName)){
+ 
+  				//$theStartTime = microtime();
+ 				$theResult = mysql_query($theQuery) or die("<b style=\"color: red;\">MySQL ERROR! ".mysql_error().", While Executing Query: <i>$theQuery</i></b>");
+ 				
+ 				//$theLastID = mysql_insert_id();
+ 				//$theEndTime = microtime();
+ 				
+ 				//$theTimeQueryTook = $theEndTime - $theStartTime;
+ 				
+ 				//TODO: Add query logging/display functions.
+ 				
+ 				mysql_close($databaseConnection);
+ 				return $theResult;
+ 			}
+ 		}else{
+ 			return "<b class=\"error\" style=\"\">MySQL ERROR! ".mysql_error().", While Executing Query: <i>$theQuery</i></b>";
+ 		}
+	}
+
+/* FUNCTIONS */
+/*###################################################################################*/
+
+/*###################################################################################*/
+/*###################################################################################*/
+/*###################################################################################*/
+/*###################################################################################*/
+/*###################################################################################*/
+
 ?>
