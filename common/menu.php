d7b832dfe169ed8e66c32d180f66f25704bd9086
Matt F <mfried@mac.com>
2011-02-02 18:30:56 -0500

    Initial Commit for Universal Menu Page
    Signed-off-by: Matt F <mfried@mac.com>

---
+++ common/menu.php
@@ -0,0 +1,14 @@
+<!-- Main nav, styled by targeting "#header nav"; you can have more than one nav element per page -->
+		<!-- The nav link semantically marks your main site navigation -->
+		<nav class="nav clear">
+			<ul>
+				<li><a href="javascript:ajaxpage('common/home.php','article');">Home</a></li>
+				<li><a href="javascript:ajaxpage('common/item.php','article');">Item</a></li>
+				<li><a href="javascript:ajaxpage('common/purchase.php','article');">Purchase</a></li>
+				<li><a href="javascript:ajaxpage('common/disposal.php','article');">Disposal</a></li>
+				<li><a href=""></a></li>
+				<li><a href="">Search</a></li>
+				<li><a href=""></a></li>
+				<li><a href="index.php?f=f">Logout</a></li>
+			</ul>
+		</nav>
