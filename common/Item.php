60b968b3c7c6abbdc1d10c940040bda778eeccf2
Matt F <mfried@mac.com>
2011-02-03 15:21:04 -0500

    Updated With lookup Functionality
    Signed-off-by: Matt F <mfried@mac.com>

--- common/Item.php
+++ common/Item.php
@@ -1,4 +1,17 @@
-<?php include_once "database.php"; ?>
+<?php 
+
+	include_once "database.php"; 
+
+	if($_GET['id'])
+	{
+		$update='T';
+		
+		print $item_sql = "SELECT * FROM inventory_view WHERE id=".$_GET['id'];
+		$result=mysql_fetch_assoc(mySQLQuery($item_sql));
+	}
+
+
+?>
 		<h2>Item</h2>
 			
 			<div class="line"></div>
@@ -7,7 +20,7 @@
 			
 			<table border="1" id="gradient-style" style="width:400px;">
 				<caption></caption>
-				<colgroup />
+				<colgroup></colgroup>
 				<colgroup span="2" title="title" /> </colgroup>
 				<thead>
 <!-- 					<tr> -->
@@ -20,7 +33,7 @@
 				</thead>
 				<tfoot>
 					<tr>
-						<th scope="col" colspan="4">1</th>
+						<th scope="col" colspan="4"><a href="javascript:ajaxpage('common/item.php?id=<?php print $_GET['id']; ?>','article');">Reload</a></th>
 					</tr>
 				</tfoot>
 				
@@ -29,34 +42,64 @@
 						<th scope="col" width="30">Type</th>
 						<td colspan="3">
 							<select size="1" name="type">
+							<?php if($update){ ?>
+								<option selected="selected" value="<?php print $result['type']; ?>"><?php print $result['type']; ?></option>
+								<option>--------------</option>
+							<?php }else{ ?>
 								<option selected="selected"></option>
+							<?php } ?>
 								<?php print type_menu(); ?>
 							</select>
 						</td>
 					</tr>
 					<tr>
 						<th scope="col">Serial #:</th>
-						<td colspan="3"><input type="text" size="40" maxlength="60"</td>
+						<td colspan="3">
+						<?php if($update){ ?>
+							<input type="text" size="50" maxlength="60" value="<?php print $result['item_serial']; ?>" />
+						<?php }else{ ?>
+							<input type="text" size="50" maxlength="60" />
+						<?php } ?>
+						</td>
 					</tr>
 					<tr>
 						<th scope="col">Manufracture:</th>
 						<td colspan="3">
-							<select size="1" name="type">
+							<select size="1" name="manufracture">
+							<?php if($update){ ?>
+								<option selected="selected" value="<?php print $result['manufacture']; ?>"><?php print $result['manufacture']; ?></option>
+								<option>--------------</option>
+							<?php }else{ ?>
 								<option selected="selected"></option>
+							<?php } ?>
 								<?php print manuf_menu(); ?>
 							</select>
 						</td>
 					</tr>
 					<tr>
 						<th scope="col">Location</th>
-						<td>1</td>
+						<td>
+							<select size="1" name="location">
+							<?php if($update){ ?>
+								<option selected="selected" value="<?php print $result['location']; ?>"><?php print $result['location']; ?></option>
+								<option>--------------</option>
+							<?php }else{ ?>
+								<option selected="selected"></option>
+							<?php } ?>
+							<?php print location_menu(); ?>
+							</select>
+						</td>
 						<th scope="col">Room</th>
-						<td>3</td>
+						<td><input type="text" size="15" maxlength="20" /></td>
 					</tr>
 					<tr>
 						<th scope="col">Notes</th>
 						<td colspan="3">
+						<?php if($update){ ?>
+							<textarea name="notes" cols="40" rows="5"><?php print $result['notes']; ?></textarea>
+						<?php }else{ ?>
 							<textarea name="notes" cols="40" rows="5"></textarea>
+						<?php } ?>
 						</td>
 					</tr>
 				</tbody>
