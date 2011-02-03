26164efc4d12c95d8a96347f1c84120a4fc786de
Matt F <mfried@mac.com>
2011-02-03 16:14:05 -0500

    Added the option for jQuery 1.5
    Signed-off-by: Matt F <mfried@mac.com>

--- common/footer.php
+++ common/footer.php
@@ -6,11 +6,14 @@
 <!-- Remote jQuery with local fallback; taken from HTML5 Boilerplate http://html5boilerplate.com -->
 <!-- jQuery version might not be the latest; check jquery.com -->
 <!-- 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script> -->
-	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.min.js"%3E%3C/script%3E'))</script>
+<!-- 	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.4.4.min.js"%3E%3C/script%3E'))</script> -->
+	
+<!-- 	<script src="http://code.jquery.com/jquery-1.5.min.js"></script> -->
+<!-- 	<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.5.min.js"%3E%3C/script%3E'))</script> -->
 
 <!-- Below is your script file, which has a basic JavaScript design pattern that you can optionally use -->
 <!-- Keep this and plugin scripts at the bottom for faster page load; combining and minifying scripts is recommended -->
-	<script src="js/general.js"></script>
+<!-- 	<script src="js/general.js"></script> -->
 
 <!-- asynchronous analytics code by Mathias Bynens; change UA-XXXXX-X to your own code; http://mathiasbynens.be/notes/async-analytics-snippet -->
 <!-- this can also be placed in the <head> if you want page views to be tracked quicker -->
