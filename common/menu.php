e7f0f3d56ffa3bae2a648e89e8d557f5e0db7d98
Matt F <mfried@mac.com>
2011-02-10 11:00:52 -0500

    Moved the menu to its own file
    It also access the database for it's permissions
    
    Signed-off-by: Matt F <mfried@mac.com>

--- common/menu.php
+++ common/menu.php
@@ -1,13 +1,36 @@
 <!-- Main nav, styled by targeting "#header nav"; you can have more than one nav element per page -->
 <!-- The nav link semantically marks your main site navigation -->
+<?php
+	include_once "database.php";
+	
+	$sql_users = "SELECT * FROM inventory_users_view WHERE username='".$_COOKIE["username"]."'";
+	
+	$result=mySQLQuery($sql_users);
+	$menu=mysql_fetch_assoc($result);
+	$name = $menu['first_name']." ".$menu['last_name'];
+
+
+/* echo $_COOKIE["username"]; */
+/* echo $HTTP_COOKIE_VARS["username"]; */
+
+?>
 <nav class="nav clear">
     <ul>
+    	
     	<li><a href="javascript:ajaxpage('common/home.php','article');">Home</a></li>
+    	<?php if ($menu['item'] == 'T') { ?>
     	<li><a href="javascript:ajaxpage('common/item.php','article');">Item</a></li>
+    	<?php } ?>
+    	<?php if ($menu['purchase'] == 'T') { ?>
     	<li><a href="javascript:ajaxpage('common/purchase.php','article');">Purchase</a></li>
+    	<?php } ?>
+    	<?php if ($menu['disposal'] == 'T') { ?>
     	<li><a href="javascript:ajaxpage('common/disposal.php','article');">Disposal</a></li>
+    	<?php } ?>
+    	<?php if ($menu['search'] == 'T') { ?>
     	<li><a href=""></a></li>
     	<li><a href="">Search</a></li>
+    	<?php } ?>
     	<li><a href=""></a></li>
     	<li><a href="index.php?f=f">Logout</a></li>
     </ul>
