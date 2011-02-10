29fc03c95902b5381901da77c0486e60230937cc
Matt F <mfried@mac.com>
2011-02-10 11:16:31 -0500

    Removed hardcoded menu for dynamic one
    Signed-off-by: Matt F <mfried@mac.com>

--- index.php
+++ index.php
@@ -41,22 +41,7 @@
 			<h2>US District Court, Southern District of New York </h2>
 			<h3>Hardware Inventory System</h3>
 		</hgroup>
-
-		<!-- Main nav, styled by targeting "#header nav"; you can have more than one nav element per page -->
-		<!-- The nav link semantically marks your main site navigation -->
-		<nav class="nav clear">
-			<ul>
-				<li><a href="javascript:ajaxpage('common/home.php','article');">Home</a></li>
-				<li><a href="javascript:ajaxpage('common/item.php','article');">Item</a></li>
-				<li><a href="javascript:ajaxpage('common/purchase.php','article');">Purchase</a></li>
-				<li><a href="javascript:ajaxpage('common/disposal.php','article');">Disposal</a></li>
-				<li><a href=""></a></li>
-				<li><a href="">Search</a></li>
-				<li><a href=""></a></li>
-				<li><a href="index.php?f=f">Logout</a></li>
-			</ul>
-		</nav>
-
+		<?php include_once "common/menu.php"; ?>
 	</header><!-- #hd1 -->
 
 	<section id="main" style="border:0 solid green;">
